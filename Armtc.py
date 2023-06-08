003fda489d0c7004168d2907d19de85b5a9b1636
Muhammad Shahroz <116105635+Shahroz135@users.noreply.github.com>
2023-06-08 17:24:12 +0500

    Add files via upload

---
+++ Armtc.py
@@ -0,0 +1,8 @@
+#program to perform arithmetic operations
+a=19
+b=87
+print("Answer of a+b=",a+b)
+print("Answer of a-b=",a-b)
+print("Answer of a*b=",a*b)
+print("Answer of a/b=",a/b)
+print("Answer of a%b=",a%b)
---
+++ Relat.py
@@ -0,0 +1,11 @@
+#Program to perform relation operations
+a=34
+b=56
+if a >= b :
+	print("a is greater than b")
+if a<=b:
+	print("a is samller than b")
+if a==b:
+	print("a is equal to b")
+if a!=b:
+	print("a and b are not equal to each other")
